25457d32cb7fcf5ccde334c45551d773d98e6bb4
Grigoriy(notebook) <grrom.for.training@yandex.ru>
2025-11-19 09:26:00 +0300

    feat: Удалить классы валидации, использовать jakarta.validation

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,37 +1,50 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.validator.UserValidator;
 
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestController
+@RequestMapping("/users")
 public class UserController {
     private final Map<Long, User> users = new HashMap<>();
 
     @GetMapping
     public Collection<User> getUsers() {
+        log.info("Получен запрос на получение всех пользователей. Текущее количество: {}", users.size());
         return users.values();
     }
 
     @PostMapping
-    public User create(@RequestBody User user) {
-        if (!UserValidator.isValid(user)) {
-            throw new ValidationException("Валидация не пройдена");
-        }
+    public User create(@Valid @RequestBody User user) {
+        log.info("Получен запрос на создание нового пользователя: {}", user);
 
         User newUser = user.toBuilder().id(getNextId()).build();
-
         users.put(newUser.getId(), newUser);
-        return user;
+
+        log.info("Пользователь успешно создан: {}", newUser);
+        return newUser;
     }
 
     @PutMapping
-    public User update(@RequestBody User user) {
+    public User update(@Valid @RequestBody User user) {
+        log.info("Получен запрос на обновление пользователя: {}", user);
+
+        if (!users.containsKey(user.getId())) {
+            String errorMessage = "Пользователь с id = " + user.getId() + " не найден";
+            log.warn("Ошибка при обновлении пользователя: {}", errorMessage);
+            throw new NotFoundException(errorMessage);
+        }
+
+        users.put(user.getId(), user);
+        log.info("Пользователь успешно обновлен: {}", user);
         return user;
     }
 
