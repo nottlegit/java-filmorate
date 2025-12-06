5a5e1f47459598bd0c391944bab4a8772cdf2d97
Grigoriy(notebook) <grrom.for.training@yandex.ru>
2025-12-06 18:50:03 +0300

    refactor: Изменить код ответа ошибок валидации с 500 на 400

--- src/main/java/ru/yandex/practicum/filmorate/config/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/config/ErrorHandler.java
@@ -1,7 +1,10 @@
 package ru.yandex.practicum.filmorate.config;
 
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,6 +16,17 @@ import ru.yandex.practicum.filmorate.exception.IllegalArgumentException;
 @Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
+    @ExceptionHandler({
+            MethodArgumentNotValidException.class,
+            ConstraintViolationException.class,
+            BindException.class
+    })
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidationExceptions(Exception e) {
+        log.debug("Validation error: {}", e.getMessage());
+        return new ErrorResponse("Ошибка валидации", e.getMessage());
+    }
+
     @ExceptionHandler({
             ValidationException.class,
             IllegalArgumentException.class
